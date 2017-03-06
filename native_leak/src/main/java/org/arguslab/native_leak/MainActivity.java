f58ebd8d21e8a5c5eb91dab40c3f049c4df9ef45
Fengguo Wei <wfg611004900521@gmail.com>
2017-03-06 12:07:51 -0500

    add one more example

---
+++ native_leak/src/main/java/org/arguslab/native_leak/MainActivity.java
@@ -0,0 +1,53 @@
+package org.arguslab.native_leak;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+
+/**
+ * @testcase_name Native_Noleak
+ * @author Fengguo Wei
+ * @author_mail fgwei521@gmail.com
+ *
+ * @description The value v of a source is sent to native lib via jni.
+ * 				native lib leaks the sensitive data.
+ * @dataflow source -> imei -> send -JNI-> Java_org_arguslab_native_1noleak_MainActivity_send -> data -> sink
+ * @number_of_leaks 1
+ * @challenges The analysis must be able to track data flow in both java and native to capture the data leakage.
+ */
+public class MainActivity extends Activity {
+
+    static {
+        System.loadLibrary("leak"); // "leak.dll" in Windows, "libleak.so" in Unixes
+    }
+
+    public static native void send(String data);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        }
+    }
+
+    private void leakImei() {
+        TelephonyManager tel = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        String imei = tel.getDeviceId(); // source
+        send(imei);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                leakImei();
+                return;
+            }
+        }
+    }
+}

dd936e249b065fbc124f072f99aab09d20f8a7d2
Fengguo Wei <wfg611004900521@gmail.com>
2017-03-06 12:07:47 -0500

    add one more example

---
+++ native_leak/src/main/jni/leak.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Fengguo Wei on 3/5/17.
+//
+
+#include <android/log.h>
+#include <jni.h>
+#include <stddef.h>
+
+
+#define LOG_TAG    "leak"
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+extern "C" {
+    JNIEXPORT void JNICALL Java_org_arguslab_native_1leak_MainActivity_send(JNIEnv *env, jobject thisObj, jstring data);
+}
+
+const char* getCharFromString(JNIEnv* env, jstring string){
+    if(string == NULL)
+        return NULL;
+
+    return  env->GetStringUTFChars(string ,0);
+}
+
+JNIEXPORT void JNICALL Java_org_arguslab_native_1leak_MainActivity_send(JNIEnv *env, jobject thisObj, jstring data) {
+    const char *charString;
+    LOGI("%s", getCharFromString(env, data)); // leak
+    return;
+}
--- native_noleak/src/main/jni/noleak.cpp
+++ native_noleak/src/main/jni/noleak.cpp
@@ -6,16 +6,16 @@
 #include <jni.h>
 
 
-#define  LOG_TAG    "noleak"
-#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOG_TAG    "noleak"
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
 extern "C" {
     JNIEXPORT void JNICALL Java_org_arguslab_native_1noleak_MainActivity_send(JNIEnv *env, jobject thisObj, jstring data);
 }
 
 JNIEXPORT void JNICALL Java_org_arguslab_native_1noleak_MainActivity_send(JNIEnv *env, jobject thisObj, jstring data) {
-    LOGE("some data");
+    LOGI("some data");
     return;
 }

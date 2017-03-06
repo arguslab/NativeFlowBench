e8f264987c135d6c5e000c996fc96288a5464bf1
Fengguo Wei <wfg611004900521@gmail.com>
2017-03-06 11:30:56 -0500

    update

---
+++ native_noleak/src/main/java/org/arguslab/native_noleak/MainActivity.java
@@ -0,0 +1,54 @@
+package org.arguslab.native_noleak;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+
+/**
+ * @testcase_name Native_Noleak
+ * @author Fengguo Wei
+ * @author_mail fgwei521@gmail.com
+ *
+ * @description The value v of a source is sent to native lib via jni.
+ * 				native lib is not leaking the data.
+ * @dataflow
+ * @number_of_leaks 0
+ * @challenges The analysis must be able to track data flow in both java and native to avoid the false positive.
+ */
+public class MainActivity extends Activity {
+
+    static {
+        System.loadLibrary("noleak"); // "noleak.dll" in Windows, "libnoleak.so" in Unixes
+    }
+
+    public static native void send(String data);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        }
+    }
+
+    private void leakImei() {
+        TelephonyManager tel = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        String imei = tel.getDeviceId(); // source
+        send(imei);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                leakImei();
+                return;
+            }
+        }
+    }
+}
---
+++ native_noleak/src/main/jni/noleak.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Fengguo Wei on 3/5/17.
+//
+
+#include <android/log.h>
+#include <jni.h>
+
+
+#define  LOG_TAG    "noleak"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+extern "C" {
+    JNIEXPORT void JNICALL Java_org_arguslab_native_1noleak_MainActivity_send(JNIEnv *env, jobject thisObj, jstring data);
+}
+
+JNIEXPORT void JNICALL Java_org_arguslab_native_1noleak_MainActivity_send(JNIEnv *env, jobject thisObj, jstring data) {
+    LOGE("some data");
+    return;
+}

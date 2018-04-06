0b56784fe87edd9445dfd59791a1c81a558a7c5d
Fengguo Wei <wfg611004900521@gmail.com>
2018-04-06 17:19:12 -0400

    add method overloading test

---
+++ native_method_overloading/src/main/java/org/arguslab/native_method_overloading/MainActivity.java
@@ -0,0 +1,43 @@
+package org.arguslab.native_method_overloading;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+
+public class MainActivity extends Activity {
+
+    static {
+        System.loadLibrary("method_overloading");
+    }
+
+    native void send(int data);
+    native void send(int[] array, String[] array2, double d, String data);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        }
+    }
+
+    private void leakImei() {
+        TelephonyManager tel = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        String imei = tel.getDeviceId(); // source
+        send(new int[1], new String[1], 0D, imei);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                leakImei();
+                return;
+            }
+        }
+    }
+}
---
+++ native_method_overloading/src/main/jni/method_overloading.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Fengguo Wei on 6/12/17.
+//
+
+#include <android/log.h>
+#include <jni.h>
+#include <stddef.h>
+
+#define LOG_TAG    "method_overloading"
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+extern "C" {
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1method_1overloading_MainActivity_send__I(JNIEnv *env, jobject thisObj, jint data);
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1method_1overloading_MainActivity_send___3I_3Ljava_lang_String_2DLjava_lang_String_2(JNIEnv *env, jobject thisObj, jintArray array, jobjectArray array2, jdouble d, jstring data);
+}
+
+const char *getCharFromString(JNIEnv *env, jstring string) {
+    if (string == NULL)
+        return NULL;
+
+    return env->GetStringUTFChars(string, 0);
+}
+
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1method_1overloading_MainActivity_send__I(JNIEnv *env, jobject thisObj, jstring data) {
+    return;
+}
+
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1method_1overloading_MainActivity_send___3I_3Ljava_lang_String_2DLjava_lang_String_2(JNIEnv *env, jobject thisObj, jintArray array, jobjectArray array2, jdouble d, jstring data) {
+    LOGI("%s", getCharFromString(env, data)); // leak
+    return;
+}
